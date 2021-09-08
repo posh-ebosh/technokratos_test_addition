0ebace7605ce448ab4a135a8f0d5f3484ff169df
posh_ebosh <sab.vkolokol@gmail.com>
2021-09-08 14:12:55 +0300

    Initial commit

---
+++ src/main/java/com/example/technokratos_test_addition/TechnokratosTestAdditionApplication.java
@@ -0,0 +1,13 @@
+package com.example.technokratos_test_addition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnokratosTestAdditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnokratosTestAdditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/technokratos_test_addition/TechnokratosTestAdditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.technokratos_test_addition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnokratosTestAdditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
